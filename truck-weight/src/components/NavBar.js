b289c4598c44e8524d73a982b3c51dc15e630877
eric <Eric355402@yahoo.com>
2019-12-05 19:44:47 -0700

    react-app, deployed on zeit, 3 basic components added, no errors

---
+++ truck-weight/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1>NavBar Component</h1>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Add Item</li>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ truck-weight/src/components/Weights.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Weights = () => {
+  return (
+    <div>
+      <h1>Truck Weight Component</h1>
+    </div>
+  );
+};
+
+export default Weights;
